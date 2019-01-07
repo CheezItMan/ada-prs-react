4130f5f111148e5bad9f82babf1aa5e2a8d68261
CheezItMan <chris@adadevelopersacademy.org>
2019-01-07 15:22:14 -0800

    starting api request

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Main extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state.classes = [];
+  }
+
+  componentDidMount() {
+
+  }
 
   render() {
     return (
