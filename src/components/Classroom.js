b8fdc5d04b04a29bd07781acce799878c55f864a
CheezItMan <chris@adadevelopersacademy.org>
2019-01-11 13:24:41 -0800

    displays classroom list

---
+++ src/components/Classroom.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import ApiWrapper from '../lib/ApiWrapper';
+
+import {REACT_APP_API_URL} from '../utils';
+
+class Classroom extends Component {
+  constructor(props) {
+    super(props);
+    console.log('props');
+    console.log(props);
+    this.state = {
+      repos: [],
+    };
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    const apiWrapper = new ApiWrapper(this.props.token);
+    console.log(`${REACT_APP_API_URL}/classes/${id}/repos`);
+
+    apiWrapper.get(`${REACT_APP_API_URL}/classes/${id}/repos`)
+      .then((response) => {
+        console.log(response);
+        const { repos } = response.data;
+
+        console.log(repos);
+
+        this.setState({
+          repos,
+        });
+      })
+      .catch((error) => {
+        console.log(error.response.status);
+        if (error.response.status === 401 ) { // unauthorized 
+          this.props.logoutCallback();
+        }
+      });
+  }
+
+  renderClassroomRepos = () => {
+    return this.state.repos.map((repo) => {
+      return (
+      <Link key={repo.id} to={`/repos/${repo.id}`}>
+        {repo.repo_url}
+      </Link>
+      );
+    });
+  }
+
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        <h3>Classroom Details</h3>
+        <ul className="classroom-repos">
+          {this.renderClassroomRepos()}
+        </ul>
+     </div>
+    )
+  }
+}
+
+export default Classroom;
